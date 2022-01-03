e953bd5c2803872a55560eac1cd7c154d919aebf
shyrokaa <jessthenimble@gmail.com>
2022-01-03 20:22:07 +0200

    Joker Fix

--- ChessObserver/ImageProcessing.h
+++ ChessObserver/ImageProcessing.h
@@ -5,6 +5,6 @@
 #include<opencv2/imgproc.hpp>
 #include<opencv2/highgui.hpp>
 
-cv::Mat  process(cv::Mat img);
-cv::Mat  process2(cv::Mat img);
-cv::Mat transformImage(cv::Mat img);
+cv::Mat  process(cv::Mat );
+cv::Mat  findLines(cv::Mat );
+cv::Mat transformImage(cv::Mat , int,int );
